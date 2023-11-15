2da006c7bfe86e87b8958ecc74f2b8e2840fe1f6
Valeriia295 <sokolsokol295@gmail.com>
2023-11-15 11:53:08 +0100

    Update 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+const markup = galleryItems
+  .map(
+    (galleryItems) => `<li class="gallery__item">
+  <a class="gallery__link" href=${galleryItems.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItems.preview}
+      data-source=${galleryItems.original}
+      alt=${galleryItems.description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+function handleClick(event) {
+  event.preventDefault();
+}
