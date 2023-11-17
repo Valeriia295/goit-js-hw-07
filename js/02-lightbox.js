e5cfc733a7bd0f3429e3c99e5f22d3e1c1d401e3
Valeriia295 <sokolsokol295@gmail.com>
2023-11-17 13:19:49 +0100

    Second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+const markup = galleryItems
+  .map(
+    (galleryItems) => `<li class="gallery__item">
+  <a class="gallery__link" href=${galleryItems.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItems.preview}
+      alt=${galleryItems.description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
